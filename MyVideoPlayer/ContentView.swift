afc6ad132d9d5d4166883f28bac3240fcec8662f
Terence Wu <terenceccwu@gmail.com>
2022-05-01 15:18:26 +0800

    Initial Commit

---
+++ MyVideoPlayer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  MyVideoPlayer
+//
+//  Created by Terence Wu on 1/5/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ MyVideoPlayer/MyVideoPlayerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyVideoPlayerApp.swift
+//  MyVideoPlayer
+//
+//  Created by Terence Wu on 1/5/2022.
+//
+
+import SwiftUI
+
+@main
+struct MyVideoPlayerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
